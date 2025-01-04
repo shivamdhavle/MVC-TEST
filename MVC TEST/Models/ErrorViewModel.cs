5dc8adc93bc3e94e8ab9661787cb4f91cc6db4ce
Shivam Dhavle <113693416+shivamdhavle@users.noreply.github.com>
2025-01-04 13:11:22 +0530

    Delete MVC TEST directory

--- MVC TEST/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MVC_TEST.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
